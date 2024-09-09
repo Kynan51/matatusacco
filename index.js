0a91c81ab77e9df988437d311a6e0d8f195b899d
Kynan51 <kenankiplimo@gmail.com>
2024-09-09 11:24:33 +0300

    create and cancel new trips

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require("express");
+const express = require("express"); // import express from express -- es6 (2015)
+
 const mysql = require("mysql")
 
 const dbconnection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "",
     database: "matatu"
 })
 
-const app = express()
+const app = express();
+
+app.use(express.static("public")); // middleware --- app.use(func) - func will executed on every request
+app.use(express.urlencoded({extended: true})) // express urlencoded
+app.use((req,res, next)=>{
+  console.log("Middleware Function") // authorization -- block some route 
+  next() 
+})
+
 app.use(express.static("public"))
 app.get("/", (req,res)=>{
     // home page/route/path
@@ -40,6 +49,14 @@ app.get("/vehicle",(req,res)=>{
             }else{
                 console.log(vehicle);
                 if(vehicle.length > 0){
+                    // get trips for this vehicle
+                    dbconnection.query(`SELECT * FROM trips JOIN routes ON trips.Route = routes.route_id WHERE Vehicle = "${req.query.plate}"`, (error, trips)=>{
+                        if(error){
+                          res.status(500).send("Server Error!!")
+                        }else{              
+                          res.render("vehicle.ejs", {vehicle: vehicle[0], trips: trips})
+                        }
+                      })  
                     res.render("vehicle.ejs", {vehicle: vehicle[0]})
                 }else{
                     res.render("vehicle.ejs", {message: "No vehicle found / invalid vehicle plate"})
@@ -52,6 +69,68 @@ app.get("/vehicle",(req,res)=>{
 
 
 
+app.get("/newtrip", (req,res)=>{
+    if(!req.query.plate){
+      res.render("newtrip.ejs", {message: "No vehicle selected"})
+    }else{
+      dbconnection.query("SELECT * FROM routes", (err, routes)=>{
+        if(err){
+          res.status(500).send("Server Error!!")
+        }else{
+          res.render("newtrip.ejs", {numberPlate: req.query.plate, routes: routes })
+        }
+      })
+    }
+  })
+  app.post("/newtrip", (req,res)=>{
+    //
+  })
+  
+  app.post("/newtrip", (req,res)=>{
+
+  console.log( req.body ); /// data in the form
+  const {numberPlate, route, departure}= req.body
+  dbconnection.query(`INSERT INTO trips(Route,Vehicle,Departure,TripStatus) VALUE(${route},"${numberPlate}", "${departure.replace("T", " ") + ":00" }","Scheduled")`, (sqlErr)=>{
+    if(sqlErr){
+      res.status(500).render("500.ejs")
+    }else{
+      res.redirect(`/vehicle?plate=${numberPlate}`)
+    }
+  } )
+})
+
+app.get("/updatetrip", (req,res)=>{
+  dbconnection.query(`UPDATE trips SET TripStatus = "${req.query.value}" WHERE trip_id = ${req.query.trip}`, (sqrErr)=>{
+    if(sqrErr){
+      res.status(500).render("500.ejs")
+    }else{
+      res.redirect(`/vehicle?plate=${req.query.plate}`)
+    }
+  })
+})
+
+app.post("/newtrip", (req,res)=>{
+
+  console.log( req.body ); /// data in the form
+  const {numberPlate, route, departure}= req.body
+  dbconnection.query(`INSERT INTO trips(Route,Vehicle,Departure,TripStatus) VALUE(${route},"${numberPlate}", "${departure.replace("T", " ") + ":00" }","Scheduled")`, (sqlErr)=>{
+    if(sqlErr){
+      res.status(500).render("500.ejs")
+    }else{
+      res.redirect(`/vehicle?plate=${numberPlate}`)
+    }
+  } )
+})
+
+app.get("/updatetrip", (req,res)=>{
+  dbconnection.query(`UPDATE trips SET TripStatus = "${req.query.value}" WHERE trip_id = ${req.query.trip}`, (sqrErr)=>{
+    if(sqrErr){
+      res.status(500).render("500.ejs")
+    }else{
+      res.redirect(`/vehicle?plate=${req.query.plate}`)
+    }
+  })
+})
 
 app.get("/owner", (req,res)=>{
     // individual owner route
